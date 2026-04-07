4a1f812e02ba60c8638bd645e67f37493416f2c2
Djembar <alokd9578@gmail.com>
2026-04-07 15:18:30 +0700

    ubah html

--- application/views/admin/v_transaksi.php
+++ application/views/admin/v_transaksi.php
@@ -4,20 +4,40 @@
         <tr style="background: #eee;">
             <th>Nama Siswa</th>
             <th>Judul Buku</th>
-            <th>Status</th>
-            <th>Aksi</th>
+            <th>Status Peminjaman</th> <th>Aksi</th>
         </tr>
         <?php foreach ($transaksi as $t): ?>
             <tr>
                 <td><?= $t->nama_lengkap; ?></td>
                 <td><?= $t->judul; ?></td>
-                <td><strong><?= strtoupper($t->status); ?></strong></td>
+                <td>
+                    <strong>
+                    <?php 
+                        if($t->status == 'menunggu') {
+                            echo "<span style='color: orange;'>Pengajuan Peminjaman</span>";
+                        } elseif($t->status == 'disetujui') {
+                            echo "<span style='color: blue;'>Sedang Dipinjam</span>";
+                        } elseif($t->status == 'pending_kembali') {
+                            echo "<span style='color: purple;'>Pengajuan Pengembalian</span>";
+                        } elseif($t->status == 'kembali') {
+                            echo "<span style='color: green;'>Sudah Dikembalikan</span>";
+                        } elseif($t->status == 'ditolak') {
+                            echo "<span style='color: red;'>DITOLAK</span>";
+                        } else {
+                            echo strtoupper($t->status);
+                        }
+                    ?>
+                    </strong>
+                </td>
                 <td>
                     <?php if ($t->status == 'menunggu'): ?>
-                        <a href="<?= base_url('admin/setuju_pinjam/' . $t->id_peminjaman . '/' . $t->id_buku); ?>">Setujui Pinjam</a>
+                        <a href="<?= base_url('admin/setuju_pinjam/' . $t->id_peminjaman . '/' . $t->id_buku); ?>" 
+                           style="color: blue; font-weight: bold;">Setujui Pinjam</a>
                     <?php elseif ($t->status == 'pending_kembali'): ?>
                         <a href="<?= base_url('admin/konfirmasi_kembali/' . $t->id_peminjaman . '/' . $t->id_buku); ?>"
-                            style="color: green;">Konfirmasi Kembali</a>
+                           style="color: green; font-weight: bold;">Konfirmasi Kembali</a>
+                    <?php else: ?>
+                        <span style="color: #999;">Tidak ada aksi</span>
                     <?php endif; ?>
                 </td>
             </tr>
